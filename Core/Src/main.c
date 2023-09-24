46c8fa785f7a4ad8914da5a2033599dca1400137
Alexis Murzeau <amubtdx@gmail.com>
2023-09-24 22:44:57 +0200

    memory: adjust mpu to avoid arm cache errata
    ARM cache in write through mode is subject to an errata.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -837,8 +837,10 @@ void MPU_Config(int enable_psram)
  HAL_MPU_ConfigRegion(&MPU_InitStruct);
 
   /* Configure the MPU attributes for PSRAM as cacheable non executable RAM
-   * Write-though no allocate offer good PSRAM performance while not slowing
-   * down audio processing too much:
+   * !!! Write-through cannot be used as Cortex-M7 has an errata on it.
+   * Write-through no allocate offer good PSRAM performance while not slowing
+   * down audio processing too much, but has an errata on Cortex M7
+   * So use no cache normal instead for minimal impact on audio.
    * mode            audio   timers
    * no cache ord    17     75
    * no cache normal 17     74
@@ -858,10 +860,10 @@ void MPU_Config(int enable_psram)
  MPU_InitStruct.Size = MPU_REGION_SIZE_512KB; // PSRAM has 1MB but only 512KB is available due to wiring.
  MPU_InitStruct.AccessPermission = enable_psram ? MPU_REGION_FULL_ACCESS : MPU_REGION_NO_ACCESS;
  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
- MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
+ MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
  MPU_InitStruct.Number = MPU_REGION_NUMBER3;
- MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+ MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
  MPU_InitStruct.SubRegionDisable = 0x00;
  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
 
