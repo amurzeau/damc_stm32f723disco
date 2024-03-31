ffff5c4fcb51c05296c8a9b43dc0adf73f34fc0c
Alexis Murzeau <amubtdx@gmail.com>
2024-03-31 17:50:50 +0200

    STM32CubeIDE: build with CMake

--- cmake/st-project.cmake
+++ cmake/st-project.cmake
@@ -1,23 +1,24 @@
 function(add_st_target_properties TARGET_NAME)
-add_custom_command(
-    TARGET ${TARGET_NAME} POST_BUILD
-    COMMAND ${CMAKE_SIZE} $<TARGET_FILE:${TARGET_NAME}>
-)
+	target_link_options(${TARGET_NAME} PRIVATE -Wl,-Map=${TARGET_NAME}.map)
+	add_custom_command(
+		TARGET ${TARGET_NAME} POST_BUILD
+		COMMAND ${CMAKE_SIZE} $<TARGET_FILE:${TARGET_NAME}>
+	)
 
-add_custom_command(
-    TARGET ${TARGET_NAME} POST_BUILD
-    COMMAND ${CMAKE_OBJCOPY} -O ihex
-    $<TARGET_FILE:${TARGET_NAME}> ${TARGET_NAME}.hex
-)
+	add_custom_command(
+		TARGET ${TARGET_NAME} POST_BUILD
+		COMMAND ${CMAKE_OBJCOPY} -O ihex
+		$<TARGET_FILE:${TARGET_NAME}> ${TARGET_NAME}.hex
+	)
 
-add_custom_command(
-    TARGET ${TARGET_NAME} POST_BUILD
-    COMMAND ${CMAKE_OBJCOPY} -O binary
-    $<TARGET_FILE:${TARGET_NAME}> ${TARGET_NAME}.bin
-)
+	add_custom_command(
+		TARGET ${TARGET_NAME} POST_BUILD
+		COMMAND ${CMAKE_OBJCOPY} -O binary
+		$<TARGET_FILE:${TARGET_NAME}> ${TARGET_NAME}.bin
+	)
 
-add_custom_command(
-    TARGET ${TARGET_NAME} POST_BUILD
-    COMMAND ${CMAKE_OBJDUMP} -h -S $<TARGET_FILE:${TARGET_NAME}> > ${TARGET_NAME}.list
-)
+	add_custom_command(
+		TARGET ${TARGET_NAME} POST_BUILD
+		COMMAND ${CMAKE_OBJDUMP} -h -S $<TARGET_FILE:${TARGET_NAME}> > ${TARGET_NAME}.list
+	)
 endfunction()
