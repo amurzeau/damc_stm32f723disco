72727aebc71bd6779d83b07477ef380184b136df
Alexis Murzeau <amubtdx@gmail.com>
2024-04-13 01:08:41 +0200

    damc: fix rare glitches on usb buffers
    There was a cache invalidate done in USB interrupt while the SAI interrupt was writing to usb circular buffer.
    This caused memory corruption as the data not yet commited to memory was thrown away by the cache invalidate.
    The fix is to not use cache management functions when not using the buffer with the DMA.

--- damc/damc_simple_lib/CodecAudio.h
+++ damc/damc_simple_lib/CodecAudio.h
@@ -30,7 +30,7 @@ private:
 	struct CodecFrame {
 		int16_t headphone[2];
 	};
-	CircularBuffer<CodecFrame, 2> out_buffer;
-	CircularBuffer<CodecFrame, 2> in_buffer;
+	CircularBuffer<CodecFrame, 2, true> out_buffer;
+	CircularBuffer<CodecFrame, 2, true> in_buffer;
 	CodecInit codecInit;
 };
