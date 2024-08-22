81546cdc29db5fc08be1b476d7439135f6d577c2
Alexis Murzeau <amubtdx@gmail.com>
2024-08-22 21:54:39 +0200

    irq: remove LCD interrupt which is not used

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -647,7 +647,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : LCD_TE_INT_Pin */
   GPIO_InitStruct.Pin = LCD_TE_INT_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(LCD_TE_INT_GPIO_Port, &GPIO_InitStruct);
 
