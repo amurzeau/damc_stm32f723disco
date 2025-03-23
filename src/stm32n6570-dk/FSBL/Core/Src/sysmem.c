2123e1539b04794f1fd2273b0a6c00fb1412a9ed
Alexis Murzeau <amubtdx@gmail.com>
2025-03-23 23:23:23 +0100

    stm32n6: fix memory usage variables

--- src/stm32n6570-dk/FSBL/Core/Src/sysmem.c
+++ src/stm32n6570-dk/FSBL/Core/Src/sysmem.c
@@ -27,7 +27,7 @@
 /**
  * Pointer to the current high watermark of the heap usage
  */
-static uint8_t *__sbrk_heap_end = NULL;
+uint8_t *__sbrk_heap_end = NULL;
 
 /**
  * @brief _sbrk() allocates memory to the newlib heap and is used by malloc
